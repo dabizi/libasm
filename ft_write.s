5e766ab1a9093e367fa995c4ca9750988a2ffd93
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-19 16:35:45 +0100

    Update ft_write.s

--- ft_write.s
+++ ft_write.s
@@ -2,6 +2,6 @@
     section __TEXT,__text
    
    _ft_write:
-              mov           rax, 0x2000004 ; Put write in the syscall register
+              mov           rax, 0x2000004 ; Put write (MacOs) in the syscall register
               syscall           ; Call the operating system to do the write function
               ret               ; Return Value
