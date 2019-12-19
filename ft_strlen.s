07141b1b5bb3a6442f4f53b772d8ed2f5d25e6e8
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-19 17:21:34 +0100

    Update ft_strlen.s

--- ft_strlen.s
+++ ft_strlen.s
@@ -2,12 +2,12 @@
     section __TEXT,__text
 
 _ft_strlen:
-        mov           rax, 0   ;
-        mov           rcx, 0   ;
+        xor eax, eax ; Could use "mov rax, 0" easier to understand but takes 7 bytes, only 2 bytes here   ;
+        xor ecx, ecx ; Same than above ("mov rcx, 0")
         
 loop:
-        mov           cl, [rdi + rax]
-        cmp           rcx, 0
+        mov           cl, [rdi + rax] ; Put rdi + rax in cl
+        cmp           rcx, 0 ; Did we reach the end of the sentence ?
         jne           newloop ; Jump if not equal, ie : if rcx != 0 which means we did not reach '\0'
         jmp           end ; If arrived here, jump to end
         
