d641dee9301cd90dfe1e93f880db71c4931bc466
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-19 16:36:12 +0100

    Update ft_read.s

--- ft_read.s
+++ ft_read.s
@@ -2,6 +2,6 @@
   section __TEXT,__text
  
  _ft_read:
-          mov rax, 0x2000003  ;   Put read in the syscall register
+          mov rax, 0x2000003  ;   Put read (MacOs) in the syscall register
           syscall             ;   Call the operating system to do the read function
           ret                 ;   Return value
