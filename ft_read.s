a251a6fd7ae886eb4f21987856a68a08ac9afe6a
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-19 16:53:43 +0100

    Update ft_read.s

--- ft_read.s
+++ ft_read.s
@@ -1,7 +1,9 @@
+  %define sys_read 0x20000003 ; Read on MacOs
+  
   global _ft_read
   section __TEXT,__text
  
  _ft_read:
-          mov rax, 0x2000003  ;   Put read (MacOs) in the syscall register
+          mov rax, sys_read  ;   Put read in the syscall register
           syscall             ;   Call the operating system to do the read function
           ret                 ;   Return value
